830055084b957f77e49f6c614973458871403a65
Jonathan Tay <jonathantaymy@gmail.com>
2022-05-23 16:47:35 +0800

    Add ChakraUI dependency

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import {ChakraProvider, CSSReset, extendTheme} from "@chakra-ui/react"
 
 import App from "./App";
+
+const theme = extendTheme({
+    // Theme extensions go here.
+})
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
     <StrictMode>
-        <App />
+        <ChakraProvider theme={theme}>
+            <CSSReset/>
+            <App/>
+        </ChakraProvider>
     </StrictMode>
 );
