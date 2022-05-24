b5633632112ec8673b710a430115353f70bace0e
Jonathan Tay <jonathantaymy@gmail.com>
2022-05-24 10:03:49 +0800

    Implement basic routing to sign-in and sign-up pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import SignUp from "./components/SignUp";
+import {Heading, Link, VStack} from "@chakra-ui/react";
+import {Link as RouterLink} from "react-router-dom"
 
 export default function App() {
-    return <SignUp/>
+    return <VStack>
+        <Heading as="h1">Landing page.</Heading>;
+        <Link as={RouterLink} to='/'>Home</Link>
+        <Link as={RouterLink} to='/sign-up'>Sign up</Link>
+        <Link as={RouterLink} to='/sign-in'>Sign in</Link>
+    </VStack>
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
-import {ChakraProvider, CSSReset, extendTheme} from "@chakra-ui/react"
-
+import {ChakraProvider, CSSReset, extendTheme, Heading} from "@chakra-ui/react"
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import App from "./App";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
 const theme = extendTheme({
     // Theme extensions go here.
@@ -11,11 +13,16 @@ const theme = extendTheme({
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-    <StrictMode>
-        <ChakraProvider theme={theme}>
-            <CSSReset/>
-            <App/>
-        </ChakraProvider>
-    </StrictMode>
-);
+root.render(<StrictMode>
+    <ChakraProvider theme={theme}>
+        <CSSReset/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="sign-up" element={<SignUp/>}/>
+                <Route path="sign-in" element={<SignIn/>}/>
+                <Route path="*" element={<Heading>404 Not Found</Heading>}/>
+            </Routes>
+        </BrowserRouter>
+    </ChakraProvider>
+</StrictMode>);
