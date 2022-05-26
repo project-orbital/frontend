d5db9bc4a18534e1b2a46450f3f005886ca8e3e6
Jonathan Tay <jonathantaymy@gmail.com>
2022-05-26 11:07:55 +0800

    Create landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import {Heading, Link, VStack} from "@chakra-ui/react";
-import {Link as RouterLink} from "react-router-dom"
+import {VStack} from "@chakra-ui/react";
+import Header from "./components/landing/Header";
+import Hero from "./components/landing/Hero";
+import FeatureCard from "./components/landing/FeatureCard";
+import Footer from "./components/landing/Footer";
+import Copyright from "./components/landing/Copyright";
 
 export default function App() {
-    return <VStack>
-        <Heading as="h1">Landing page.</Heading>;
-        <Link as={RouterLink} to='/'>Home</Link>
-        <Link as={RouterLink} to='/sign-up'>Sign up</Link>
-        <Link as={RouterLink} to='/sign-in'>Sign in</Link>
+    return <VStack h="full" w="full" spacing="0px">
+        <Header/>
+        <Hero
+            title="Eye-catching hero header text, in two lines!"
+            subtitle="Hero description goes here, it’ll probably
+            run longer so that it takes up more than one line. In fact, it might even span three lines!"
+        />
+        <VStack p="50px 25%" bg="white" spacing="25px">
+            <FeatureCard/>
+            <FeatureCard/>
+            <FeatureCard/>
+        </VStack>
+        <Footer text="This is a work in progress."/>
+        <Copyright/>
     </VStack>
 }
---
+++ src/components/landing/Footer.js
@@ -0,0 +1,9 @@
+import {Center, Text} from "@chakra-ui/react";
+
+export default function Footer(props) {
+    return <Center w="100%" h="400px" p="40px" bg="gray.500">
+        <Text fontSize="6xl" color="white">
+            {props.text}
+        </Text>
+    </Center>
+}
