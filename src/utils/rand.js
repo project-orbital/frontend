09b79c05ea18a97c83acd120649a0bbe3af2871a
Jonathan Tay <jonathantaymy@gmail.com>
2022-05-27 16:34:10 +0800

    Add random int function

--- src/utils/rand.js
+++ src/utils/rand.js
@@ -1,9 +1,11 @@
-import {faker} from "@faker-js/faker";
+export function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 export function randIntGenerator(min, max) {
-    return () => Math.floor(Math.random() * (max - min) + min);
+    return () => randInt(min, max);
 }
 
 export function randIntArray(len, min, max) {
-    return Array.from({length: len}, () => faker.datatype.number({min, max}));
+    return Array.from({length: len}, () => randInt(min, max));
 }
