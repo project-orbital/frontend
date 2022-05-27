5344b9dc2817c43a27fd701954b00877599a2b28
Jonathan Tay <jonathantaymy@gmail.com>
2022-05-27 15:14:07 +0800

    Change root rendering to React 17 style

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import * as ReactDOM from "react-dom";
 import {ChakraProvider, CSSReset, extendTheme, Heading} from "@chakra-ui/react"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import App from "./App";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import Dashboard from "./components/dashboard/Dashboard";
 
 const theme = extendTheme({
     // Theme extensions go here.
 })
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<StrictMode>
+const element = <StrictMode>
     <ChakraProvider theme={theme}>
         <CSSReset/>
         <BrowserRouter>
@@ -21,8 +19,11 @@ root.render(<StrictMode>
                 <Route path="/" element={<App/>}/>
                 <Route path="sign-up" element={<SignUp/>}/>
                 <Route path="sign-in" element={<SignIn/>}/>
+                <Route path="dashboard" element={<Dashboard/>}/>
                 <Route path="*" element={<Heading>404 Not Found</Heading>}/>
             </Routes>
         </BrowserRouter>
     </ChakraProvider>
-</StrictMode>);
+</StrictMode>;
+
+ReactDOM.render(element, document.getElementById('root'));
