4420a7f8f01bab1853f4169d1ae68d5a7c7c77dd
Jonathan Tay <jonathantaymy@gmail.com>
2022-05-27 17:33:35 +0800

    Replace hardcoded text with faker.lorem

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Hero from "./components/landing/Hero";
 import FeatureCard from "./components/landing/FeatureCard";
 import Footer from "./components/landing/Footer";
 import Copyright from "./components/landing/Copyright";
+import {faker} from "@faker-js/faker";
 
 export default function App() {
     return <VStack h="full" w="full" spacing="0px">
         <Header/>
         <Hero
-            title="Eye-catching hero header text, in two lines!"
-            subtitle="Hero description goes here, it’ll probably
-            run longer so that it takes up more than one line. In fact, it might even span three lines!"
+            title="Eye-catching call to action, in two lines!"
+            subtitle={faker.lorem.sentences(2)}
         />
         <VStack p="50px 25%" bg="white" spacing="25px">
             <FeatureCard/>
             <FeatureCard/>
             <FeatureCard/>
         </VStack>
-        <Footer text="This is a work in progress."/>
+        <Footer text="This is entire page is a work in progress. Stay tuned!"/>
         <Copyright/>
     </VStack>
 }
--- src/components/landing/FeatureCard.js
+++ src/components/landing/FeatureCard.js
@@ -1,21 +1,18 @@
 import {Box, Heading, LinkBox, LinkOverlay, Text} from "@chakra-ui/react";
+import {faker} from "@faker-js/faker";
 
 export default function FeatureCard() {
     return <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md'>
-        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
-            13 days ago
-        </Box>
         <Heading size='md' my='2'>
             <LinkOverlay href='#'>
-                New Year, New Beginnings: Smashing Workshops & Audits
+                {faker.lorem.sentence()}
             </LinkOverlay>
         </Heading>
         <Text mb='3'>
-            Catch up on what’s been cookin’ at Smashing and explore some of the most
-            popular community resources.
+            {faker.lorem.paragraph()}
         </Text>
         <Box as='a' color='teal.400' href='#' fontWeight='bold'>
-            Some inner link
+            {faker.lorem.sentence()}
         </Box>
     </LinkBox>
 }
