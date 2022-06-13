b1ce86d7bc1e26c944d8bea3bfbe4ad5c8f9d423
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-13 16:23:55 +0800

    Modify Table to accept arrays of objects instead of 2D arrays

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,27 +1,63 @@
 import {Table as ChakraTable, TableContainer, Tbody, Td, Th, Thead, Tr} from "@chakra-ui/react";
 
 /**
- * Dynamic table component.
+ * Wrapper component for creating `ChakraTable`s from with data.
  *
- * Props:
- * - headers: An array of strings which form the headers of each column.
- * - rows: The number of rows to limit the table to.
- * - body: A 2D array of strings, where elements in the outer array form a row,
- *         and elements in the nested arrays form cells of their respective row.
+ * The table is populated from an array of objects, each object forming a row,
+ * with its keys contributing to the table headers and its values as cells.
+ *
+ * e.g. [{a: 1, b: 2}, {b: 3, c: 4}] creates a table with 2 rows and 3 columns:
+ * | a    | b    | c    |
+ * | 1    | 2    | null |
+ * | null | 3    | 4    |
+ *
+ * @param values the array of objects
+ * @param rowLimit an optional maximum number of table rows to create
+ * @return a new, populated table component
  */
-export default function Table(props) {
+export default function Table({values, rowLimit}) {
+    // Get and de-duplicate all the keys to use them as headers.
+    const headers = [...new Set(values.map(obj => obj.keys))];
+
+    // Converts an object to an array of its values with respect to the headers.
+    // If the object does not have a header element as one of its keys, that cell will be null.
+    const objToRow = obj => headers.map(header => obj.get(header, null));
+
     return <TableContainer w="100%">
         <ChakraTable size="sm" variant="striped" placement="top">
-            <Thead>
-                <Tr>
-                    {props.headers.map(header => <Th>{header}</Th>)}
-                </Tr>
-            </Thead>
-            <Tbody>
-                {props.body && props.body
-                    .slice(0, props.rows)
-                    .map(row => <Tr>{row.map(cell => <Td>{cell}</Td>)}</Tr>)}
-            </Tbody>
+            {createTableHeaders(headers)}
+            {createTableBody(values.map(objToRow), rowLimit)}
         </ChakraTable>
     </TableContainer>
+}
+
+/**
+ * Creates a table header row from an array.
+ *
+ * @param headers an array of strings to create headers from
+ * @return the `<Thead>` table header JSX element
+ */
+function createTableHeaders(headers) {
+    return <Thead>
+        <Tr>
+            {headers.map((header, x) => <Th key={x}>{header}</Th>)}
+        </Tr>
+    </Thead>
+}
+
+/**
+ * Creates a table body from a 2D array.
+ *
+ * @param body a 2D array to populate the table in row-major order
+ * @param rowLimit an optional maximum number of table rows to create
+ * @return the `<Tbody>` table body JSX element
+ */
+function createTableBody(body, rowLimit) {
+    if (!body || rowLimit <= 0) {
+        return <Tbody></Tbody>
+    }
+    const rowToJSX = (row, y) => <Tr key={y}>{row.map((cell, x) => <Td key={x}>{cell}</Td>)}</Tr>
+    return <Tbody>
+        {body.slice(0, rowLimit || body.length).map(rowToJSX)}
+    </Tbody>
 }
