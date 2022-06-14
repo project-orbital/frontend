5c0a86c9b06f30f681626d8ffa8661b2cab5169d
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-14 12:08:01 +0800

    Integrate React Redux

--- src/index.js
+++ src/index.js
@@ -6,24 +6,28 @@ import App from "./App";
 import SignUp from "./routes/SignUp";
 import SignIn from "./routes/SignIn";
 import Dashboard from "./routes/Dashboard";
+import store from "./states/store";
+import {Provider} from "react-redux";
 
 const theme = extendTheme({
     // Theme extensions go here.
 })
 
 const element = <StrictMode>
-    <ChakraProvider theme={theme}>
-        <CSSReset/>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App/>}/>
-                <Route path="sign-up" element={<SignUp/>}/>
-                <Route path="sign-in" element={<SignIn/>}/>
-                <Route path="dashboard" element={<Dashboard/>}/>
-                <Route path="*" element={<Heading>404 Not Found</Heading>}/>
-            </Routes>
-        </BrowserRouter>
-    </ChakraProvider>
+    <Provider store={store}>
+        <ChakraProvider theme={theme}>
+            <CSSReset/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App/>}/>
+                    <Route path="sign-up" element={<SignUp/>}/>
+                    <Route path="sign-in" element={<SignIn/>}/>
+                    <Route path="dashboard" element={<Dashboard/>}/>
+                    <Route path="*" element={<Heading>404 Not Found</Heading>}/>
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    </Provider>
 </StrictMode>;
 
 ReactDOM.render(element, document.getElementById('root'));
