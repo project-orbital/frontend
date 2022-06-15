c9960f3cb5e214c2dabaaa02d641f77a16193ce1
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-15 10:01:19 +0800

    Fix sidebar scrolling with body content

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,27 +3,25 @@ import SidebarLink from "./SidebarLink";
 import SidebarCategory from "./SidebarCategory";
 
 export default function Sidebar() {
-    return <VStack p="40px" bg="gray.800">
+    return <VStack p="40px" minH="100vh" w="260px" zIndex={999} pos="fixed" bgColor="gray.800">
         <Heading as="h1" size="lg" color="white">DollarPlanner</Heading>;
         <VStack h="100%" pt="85px" spacing="10px" align="start" alignSelf="start">
             <SidebarLink to="/dashboard" text="Dashboard"/>
             <SidebarLink to="/dashboard" text="Analyze"/>
             <SidebarLink to="/dashboard" text="Plan"/>
             <SidebarLink to="/dashboard" text="Learn"/>
+            <SidebarCategory name="Bank Accounts"
+                             items={[<SidebarLink to="/dashboard" text="Account 1" size="sm"/>,
+                                 <SidebarLink to="/dashboard" text="Account 2" size="sm"/>,
+                                 <SidebarLink to="/dashboard" text="Account 3" size="sm"/>]}/>
 
-            <SidebarCategory name="Bank Accounts" items={[
-                <SidebarLink to="/dashboard" text="Account 1" size="sm"/>,
-                <SidebarLink to="/dashboard" text="Account 2" size="sm"/>,
-                <SidebarLink to="/dashboard" text="Account 3" size="sm"/>
-            ]}/>
-
-            <SidebarCategory name="Investment Accounts" items={[
-                <SidebarLink to="/dashboard" text="Account 1" size="sm"/>,
-                <SidebarLink to="/dashboard" text="Account 2" size="sm"/>,
-                <SidebarLink to="/dashboard" text="Account 3" size="sm"/>
-            ]}/>
-
-            <Spacer/>
+            <SidebarCategory name="Investment Accounts"
+                             items={[<SidebarLink to="/dashboard" text="Account 1" size="sm"/>,
+                                 <SidebarLink to="/dashboard" text="Account 2" size="sm"/>,
+                                 <SidebarLink to="/dashboard" text="Account 3" size="sm"/>]}/>
+        </VStack>
+        <Spacer/>
+        <VStack h="100%" pt="85px" spacing="10px" align="start" alignSelf="start">
             <SidebarLink to="/dashboard" text="Settings"/>
             <SidebarLink to="/" text="Sign out"/>
         </VStack>
