0bc3bc2839c4341f44a5f10349e6fd61a299c4aa
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-17 16:41:30 +0800

    Use new `Breadcrumbs` in `Dashboard`

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,10 +1,11 @@
 import Sidebar from "../components/sidebar/Sidebar";
-import {Breadcrumb, BreadcrumbItem, BreadcrumbLink, Grid, GridItem, Heading, HStack, VStack} from "@chakra-ui/react";
+import {Grid, GridItem, HStack, VStack} from "@chakra-ui/react";
 
 import * as rand from "../utils/rand";
 import TransactionsCard from "../components/dashboard/TransactionsCard";
 import NetWorthCard from "../components/dashboard/NetWorthCard";
 import AssetAllocationCard from "../components/dashboard/AssetAllocationCard";
+import Breadcrumbs from "../components/visuals/Breadcrumbs";
 
 // === === ===
 // Artificial data.
@@ -12,9 +13,9 @@ const rng = rand.randIntGenerator(20000, 70000);
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const netWorthData = months.map(month => ({key: month, value: rng()}));
 const netWorthValue = netWorthData[11].value;
-const netWorthChange = ((netWorthValue - netWorthData[10].value) / netWorthData[10].value * 100).toFixed(2)
+const netWorthChange = ((netWorthValue - netWorthData[10].value) / netWorthData[10].value * 100).toFixed(2);
 
-const assets = ["Cash", "Bonds", "Stocks", "Crypto"]
+const assets = ["Cash", "Bonds", "Stocks", "Crypto"];
 const assetsRaw = rand.randIntArray(assets.length, 0, 100);
 const assetsTotal = assetsRaw.reduce((a, b) => a + b);
 const assetsData = assets.map((asset, i) => ({
@@ -22,25 +23,16 @@ const assetsData = assets.map((asset, i) => ({
     value: Math.floor(netWorthValue * assetsRaw[i] / assetsTotal)
 }));
 
-const Breadcrumbs = () => <>
-    <Breadcrumb spacing='8px' separator="/">
-        <BreadcrumbItem>
-            <BreadcrumbLink href='#'>Home</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbItem>
-            <BreadcrumbLink href='#'>Dashboard</BreadcrumbLink>
-        </BreadcrumbItem>
-    </Breadcrumb>
-</>
-
 export default function Dashboard() {
-    return <HStack w='100%' minH='100vh' align='start' spacing='0' overflow='hidden' float='left' bg='gray.200'>
-        <Sidebar selected='dashboard'/>
+    return <HStack w="100%" minH="100vh" align="start" spacing="0" overflow="hidden" float="left" bg="gray.200">
+        <Sidebar selected="dashboard"/>
         {/* Addition of left padding to shift the body content right by 160px, the width of the sidebar. */}
-        <VStack w='100%' p='40px' pl='200px' align='stretch' overflow='auto'>
-            <Breadcrumbs/>
-            <Heading as="h1">Dashboard</Heading>
-            <Grid w='100%' pt='40px' gap='25px' autoColumns='minmax(600px, auto)' autoFlow='row'>
+        <VStack w="100%" p="40px" pl="200px" align="stretch" overflow="auto">
+            <Breadcrumbs
+                path="Home/Dashboard"
+                links={["/dashboard", "/dashboard"]}
+            />
+            <Grid w="100%" pt="40px" gap="25px" autoColumns="minmax(600px, auto)" autoFlow="row">
                 <GridItem>
                     <NetWorthCard value={netWorthValue} change={netWorthChange} data={netWorthData}/>
                 </GridItem>
@@ -52,5 +44,5 @@ export default function Dashboard() {
                 </GridItem>
             </Grid>
         </VStack>
-    </HStack>
+    </HStack>;
 }
