143c9b5a141fa5202704e6be36d676cdb480568e
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-18 15:14:41 +0800

    Refactor to take advantage of `Card`'s grid

--- src/components/accounts/AccountCard.js
+++ src/components/accounts/AccountCard.js
@@ -1,29 +1,25 @@
 import Card from "../visuals/Card";
 import UploadButton from "../upload/UploadButton";
-import {SimpleGrid, Spacer} from "@chakra-ui/react";
 
 export default function AccountCard({account, index}) {
     return <Card
         info={index}
         heading={account.name}
         subheading={account.nickname}
-        link="/dashboard"
+        link={`/accounts/${account.id}`}
     >
-        <SimpleGrid minChildWidth="300px" spacing="10px">
-            <Card
-                isNested
-                heading="Balance"
-                subheading="SGD 28,381.23"
-            >
-            </Card>
-            <Spacer/>
-            <Card
-                isNested
-                heading="Transactions"
-                subheading="No transactions to display."
-            >
-                <UploadButton/>
-            </Card>
-        </SimpleGrid>
+        <Card
+            isNested
+            heading="Balance"
+            subheading="SGD 28,381.23"
+        >
+        </Card>
+        <Card
+            isNested
+            heading="Transactions"
+            subheading="No transactions to display."
+        >
+            <UploadButton/>
+        </Card>
     </Card>;
 }
