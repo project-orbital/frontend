f3f29934b1d33249b8ee752b2971555cf7f9e9f3
huangzz125 <huangzz125@gmail.com>
2022-06-21 03:19:15 +0800

    changed work in progress footer to orbital banner

--- src/features/landing/components/Copyright.js
+++ src/features/landing/components/Copyright.js
@@ -1,21 +1,17 @@
-import { HStack, Spacer, Text } from "@chakra-ui/react";
+import { Center, Text } from "@chakra-ui/react";
 
 export default function Copyright() {
     return (
-        <HStack
+        <Center
             w="100%"
             p="1em 20%"
             justify="space-between"
             bg="gray.800"
             shadow="2xl"
         >
-            <Text fontSize="xs" color="white">
+            <Text fontSize="s" color="white">
                 Copyright © 2022 DollarPlanner. All rights reserved.
             </Text>
-            <Spacer />
-            <Text fontSize="xs" color="white">
-                An Orbital 2022 Project @ NUS School of Computing
-            </Text>
-        </HStack>
+        </Center>
     );
 }
