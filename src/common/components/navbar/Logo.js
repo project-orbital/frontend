902ab19c819ceb7d74d049905539df19555061dc
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-25 17:18:57 +0800

    Add logo to sidebar

--- src/common/components/navbar/Logo.js
+++ src/common/components/navbar/Logo.js
@@ -1,16 +1,27 @@
 import { BsCurrencyExchange } from "react-icons/bs";
-import { Box, Heading, HStack } from "@chakra-ui/react";
+import { Box, Heading, Stack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-export default function Logo() {
+export default function Logo({ direction, color, size, link, hasBorder }) {
     return (
-        <HStack as={Link} to="/">
-            <Box color="fg">
+        <Stack
+            as={Link}
+            to={link || "/"}
+            direction={direction || "row"}
+            align="center"
+        >
+            <Box color={color || "fg"}>
                 <BsCurrencyExchange size="30px" />
             </Box>
-            <Heading color="fg" size="lg" fontWeight="bold">
+            <Heading
+                color={color || "fg"}
+                size={size || "lg"}
+                fontWeight="bold"
+                pb={hasBorder && "15px"}
+                borderBottom={hasBorder && "1.5px solid white"}
+            >
                 DollarPlanner
             </Heading>
-        </HStack>
+        </Stack>
     );
 }
