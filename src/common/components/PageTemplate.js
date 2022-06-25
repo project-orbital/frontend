7a642244fdd5cc0d51999ddc3ec9e6f2133537a5
Jonathan Tay <jonathantaymy@gmail.com>
2022-06-25 12:20:19 +0800

    Add `PageTemplate` variant for authentication pages

--- src/common/components/PageTemplate.js
+++ src/common/components/PageTemplate.js
@@ -1,5 +1,7 @@
 import { Box, VStack } from "@chakra-ui/react";
 import Sidebar from "./sidebar/Sidebar";
+import Card from "./Card";
+import Navbar from "./navbar/Navbar";
 
 /**
  * Template component for a page with the sidebar included.
@@ -7,11 +9,30 @@ import Sidebar from "./sidebar/Sidebar";
  * The children elements of the page will be vertically stacked.
  * A typical body of a page includes a breadcrumb trail before the rest of the content.
  *
+ * @param variant "auth" for authentication pages, "app" for application pages
+ * @param heading the heading only used for the authentication pages
  * @param children the children elements of the page
  * @param page the page that will be selected in the sidebar
  * @return the page populated with the sidebar and children elements
  */
-export default function PageTemplate({ children, page }) {
+export default function PageTemplate({ variant, heading, children, page }) {
+    if (variant === "auth") {
+        return (
+            <VStack spacing="0" bg="bg">
+                <Navbar />
+                <VStack justify="center" minH="100vh" spacing="0" w="60%">
+                    <Card
+                        isCentered
+                        isDarkModeReady
+                        isStandalone
+                        heading={heading}
+                    >
+                        {children}
+                    </Card>
+                </VStack>
+            </VStack>
+        );
+    }
     return (
         <Box>
             <Sidebar selected={page} />
