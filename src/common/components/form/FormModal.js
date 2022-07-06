53ce7fc2cb05ea15c0cd9eb8ee2282b8ed48c020
Jonathan Tay <jonathantaymy@gmail.com>
2022-07-06 11:52:46 +0800

    Create `FormModal` component for abstraction

---
+++ src/common/components/form/FormModal.js
@@ -0,0 +1,46 @@
+import Modal from "../Modal";
+import { Form, Formik } from "formik";
+import { VStack } from "@chakra-ui/react";
+
+/**
+ * A modal with an integrated form constructed from a vertical stack of its children elements.
+ * Uses Formik and Yup to create and validate the form respectively.
+ * The parent modal can be customized with the props in `Modal`.
+ *
+ * The `initialValues` prop is required, otherwise a warning that an uncontrolled input
+ * is being changed to be controlled will be emitted.
+ *
+ * The `onSubmit` prop is also required. Navigation to close the modal should
+ * be placed in this callback, otherwise the modal will not close.
+ * The callback should be a function that accepts the form values as an argument.
+ *
+ * The `validationSchema` expects a Yup object with the form fields as keys.
+ *
+ * @param initialValues the initial values of the form
+ * @param validationSchema the validation schema for the form
+ * @param onSubmit the callback to call when the form is submitted
+ * @param children the components of the form
+ * @param modalProps the props to pass to the parent modal
+ * @return {JSX.Element}
+ */
+export default function FormModal({
+    initialValues,
+    validationSchema,
+    onSubmit,
+    children,
+    ...modalProps
+}) {
+    return (
+        <Modal {...modalProps} submitForm="integrated-form">
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                <Form id="integrated-form">
+                    <VStack spacing="20px">{children}</VStack>
+                </Form>
+            </Formik>
+        </Modal>
+    );
+}
