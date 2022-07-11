0fbc097ec4f46f49824722b27ee32028f48feaec
Jonathan Tay <jonathantaymy@gmail.com>
2022-07-11 11:30:16 +0800

    Fix duplicate toasts on sign-out

--- src/features/user/authentication/SignOut.js
+++ src/features/user/authentication/SignOut.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import ky from "ky";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "@chakra-ui/react";
@@ -7,29 +7,40 @@ import LandingPage from "../../landing/LandingPage";
 export default function SignOut() {
     const navigate = useNavigate();
     const toast = useToast();
+    const id = "sign-out";
 
-    useEffect(() => {
+    const signOut = useCallback(() => {
         ky.get(`${process.env.REACT_APP_BACKEND}/users/sign-out`, {
             credentials: "include",
         })
             .then(() => {
-                toast({
-                    title: "Goodbye!",
-                    description: "You have signed out successfully.",
-                    status: "success",
-                    isClosable: true,
-                });
+                if (!toast.isActive(id)) {
+                    toast({
+                        id,
+                        title: "Goodbye!",
+                        description: "You have signed out successfully.",
+                        status: "success",
+                        isClosable: true,
+                    });
+                }
                 navigate("/");
             })
             .catch(() => {
-                toast({
-                    title: "Goodbye!",
-                    description: "You have signed out unsuccessfully.",
-                    status: "error",
-                    isClosable: true,
-                });
+                if (!toast.isActive(id)) {
+                    toast({
+                        id,
+                        title: "Goodbye!",
+                        description: "You have signed out unsuccessfully.",
+                        status: "error",
+                        isClosable: true,
+                    });
+                }
                 navigate("/");
             });
-    });
+    }, [navigate, toast]);
+
+    useEffect(() => {
+        signOut();
+    }, [signOut]);
     return <LandingPage />;
 }
