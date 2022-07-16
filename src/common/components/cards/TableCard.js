bc15d71165daab01bf45dd3f4b2adb8b45f401e9
Jonathan Tay <jonathantaymy@gmail.com>
2022-07-16 12:28:53 +0800

    Refactor `Table` and `TableCard` to accept headers and `isNumeric`

--- src/common/components/cards/TableCard.js
+++ src/common/components/cards/TableCard.js
@@ -1,10 +1,23 @@
 import BaseCard from "./BaseCard";
 import Table from "../visuals/Table";
 
-export default function TableCard({ values, rowLimit, children, ...props }) {
+export default function TableCard({
+    values,
+    headers,
+    isNumeric,
+    rowLimit,
+    children,
+    ...props
+}) {
     return (
         <BaseCard {...props} px={0} py={4} spacing={0}>
-            <Table values={values} rowLimit={rowLimit} />
+            <Table
+                values={values}
+                headers={headers}
+                isNumeric={isNumeric}
+                rowLimit={rowLimit}
+                offset={6}
+            />
             {children}
         </BaseCard>
     );
