d78068035ec67adb0f5c53fa5bba5249a7b8fa41
Jonathan Tay <jonathantaymy@gmail.com>
2022-07-17 16:35:42 +0800

    Increase test timeout duration

--- playwright.config.js
+++ playwright.config.js
@@ -13,13 +13,13 @@ const { devices } = require("@playwright/test");
  */
 const config = {
     testDir: "./tests" /* Maximum time one test can run for. */,
-    timeout: 10 * 1000,
+    timeout: 30 * 1000,
     expect: {
         /**
          * Maximum time expect() should wait for the condition to be met.
          * For example in `await expect(locator).toHaveText();`
          */
-        timeout: 1000,
+        timeout: 5000,
     } /* Run tests in files in parallel */,
     fullyParallel: true /* Fail the build on CI if you accidentally left test.only in the source code. */,
     forbidOnly: !!process.env.CI /* Retry on CI only */,
