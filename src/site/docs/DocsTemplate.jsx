4cb97b2bedb9c6d9ab71dcdbb458243a465fa846
Jonathan Tay <jonathantaymy@gmail.com>
2022-07-18 16:16:56 +0800

    Modify template and create links

--- src/site/docs/DocsTemplate.jsx
+++ src/site/docs/DocsTemplate.jsx
@@ -7,9 +7,12 @@ import {
     DrawerContent,
     DrawerHeader,
     DrawerOverlay,
+    Flex,
     IconButton,
-    Link,
+    Link as ChakraLink,
     Show,
+    Stack,
+    Text,
     useDisclosure,
     VStack,
 } from "@chakra-ui/react";
@@ -17,30 +20,88 @@ import { HamburgerIcon } from "@chakra-ui/icons";
 import Navbar from "../../common/components/navbar/Navbar";
 import { useEffect } from "react";
 
+const Section = ({ name, children }) => {
+    return (
+        <Stack direction="column" spacing={2}>
+            <Text
+                fontSize="xs"
+                fontWeight="bold"
+                textTransform="uppercase"
+                color="gray.500"
+                pt={10}
+            >
+                {name}
+            </Text>
+            {children}
+        </Stack>
+    );
+};
+const Link = ({ text, ...props }) => (
+    <ChakraLink
+        fontSize="sm"
+        as={RouterLink}
+        fontWeight="medium"
+        color="fg-light"
+        {...props}
+    >
+        {text}
+    </ChakraLink>
+);
+
 const SidebarContent = () => {
     return (
         <VStack align="start">
-            <Link as={RouterLink} to="./">
-                Introduction
-            </Link>
+            <Section name="Overview">
+                <Link to="./" text="Introduction" />
+                <Link to="./overview/quick-links" text="Quick Links" />
+                <Link to="./overview/tech-stack" text="Tech Stack" />
+            </Section>
+            <Section name="Features">
+                <Link to="./features/milestone-1" text="Milestone I" />
+                <Link to="./features/milestone-2" text="Milestone II" />
+                <Link to="./features/milestone-3" text="Milestone III" />
+            </Section>
+            <Section name="Architecture">
+                <Link
+                    to="./architecture/authentication"
+                    text="Authentication"
+                />
+                <Link to="./architecture/database" text="Database" />
+                <Link to="./architecture/parser" text="Parser" />
+            </Section>
+            <Section name="Design">
+                <Link to="./design/ui-ux" text="UI & UX" />
+            </Section>
+            <Section name="Testing">
+                <Link to="./testing/strategy" text="Strategy" />
+                <Link to="./testing/user-testing" text="User Testing" />
+            </Section>{" "}
+            <Section name="Miscellany">
+                <Link to="./miscellany/swe-practices" text="SWE Practices" />
+                <Link to="./miscellany/limitations" text="Limitations" />
+                <Link to="./miscellany/project-log" text="Project Log" />
+            </Section>
         </VStack>
     );
 };
 
 // Sidebar variant which will be displayed inline with the content.
 const SidebarInline = () => (
-    <VStack
+    <Flex
         pos="fixed"
-        w={[0, 0, 0, 250, 300]}
         top={[20, 24]}
-        pt={8}
+        w={[0, 0, 0, 250, 270]}
+        h={["calc(100vh - 5rem)", "calc(100vh - 6rem)"]}
         pl={[0, 0, 0, 16]}
+        pb={[20, 24]}
         zIndex={4}
         align="start"
+        overflowY="auto"
+        flex={1}
         // border="1px solid red"
     >
         <SidebarContent />
-    </VStack>
+    </Flex>
 );
 
 // Sidebar variant which will be tucked away in a drawer.
@@ -48,8 +109,8 @@ const SidebarDrawer = ({ isOpen, onClose }) => (
     <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
         <DrawerContent>
-            <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
-            <DrawerBody>
+            <DrawerHeader borderBottomWidth="1px">Documentation</DrawerHeader>
+            <DrawerBody pb={16}>
                 <SidebarContent />
             </DrawerBody>
         </DrawerContent>
@@ -104,8 +165,9 @@ export default function DocsTemplate() {
             <Container
                 pos="absolute"
                 top={[20, 24, 28]}
-                left={[0, 0, 0, 250, 300]}
-                px={[4, 10, 20, 0, 0, 0]}
+                left={[0, 0, 0, 300, 320]}
+                px={[4, 10, 20, 0]}
+                pb={[20, 24]}
             >
                 <Outlet />
             </Container>
