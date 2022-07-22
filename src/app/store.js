51d0b61e779be775ed506bfc08925e2969d7671f
Jonathan Tay <jonathantaymy@gmail.com>
2022-07-22 20:52:33 +0800

    Delete `budgets` reducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,6 @@ import { persistReducer, persistStore } from "redux-persist"; // defaults to loc
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "./api";
 import filesReducer from "../features/accounts/state/files";
-import budgetsReducer from "../features/plan/state/budgets";
 import contributionsReducer from "../features/learn/state/contributions";
 import preferencesReducer from "../features/settings/state/preferences";
 import storage from "redux-persist/lib/storage";
@@ -23,7 +22,6 @@ const persistConfig = {
 const combinedReducer = combineReducers({
     files: filesReducer,
     preferences: preferencesReducer,
-    budgets: budgetsReducer,
     contributions: contributionsReducer,
     // Add the generated reducer as a specific top-level slice.
     [api.reducerPath]: api.reducer,
